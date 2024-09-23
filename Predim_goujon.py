1bc0aa2a155bb30890a9e7a3742208b3389d49a5
elie.tisseur <elie.tisseur@cohb-industrie.com>
2024-09-23 10:32:32 +0200

    V2

--- Predim_goujon.py
+++ Predim_goujon.py
@@ -7,7 +7,7 @@ lien_image_delta = "Imagedelta.png"
 lien_image_Vrds = "Image_vrds.png"
 lien_image_sem = "Image_semelle.png"
 
-st.set_page_config(layout="wide", initial_sidebar_state ="expanded")
+st.set_page_config(layout="wide", initial_sidebar_state ="expanded", page_title= "Predim Aquaman")
 st.title("Outil de prédimensionnement (V.Git_V2)")
 
 with st.sidebar:
